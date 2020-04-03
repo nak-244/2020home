9f107f8495551cfb42de94047f5290db5fed3922
nak-244 <t09024656860@gmail.com>
2020-04-03 14:30:38 +0900

    Update content-single-job_listing.php

--- wp-job-board/templates/content-single-job_listing.php
+++ wp-job-board/templates/content-single-job_listing.php
@@ -18,7 +18,7 @@ global $post;
 			<?php do_action( 'wp_job_board_before_job_content', $post->ID ); ?>
 			<!-- job descriptionnn -->
 			<div class="job-detail-description">
-				<h3><?php esc_html_e('Job Description', 'wp-job-board'); ?></h3>
+				<h3>あ<?php esc_html_e('Job Description', 'wp-job-board'); ?></h3>
 				<div class="inner">
 					<?php the_content(); ?>
 				</div>
