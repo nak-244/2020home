27d8ee48ebb1bb0fdba426624faf19938424b1c2
nak-244 <t09024656860@gmail.com>
2020-04-03 14:55:50 +0900

    Update content-single-job_listing-v2.php

--- careerup/template-jobs/content-single-job_listing-v2.php
+++ careerup/template-jobs/content-single-job_listing-v2.php
@@ -20,8 +20,8 @@ global $post;
 			<?php echo WP_Job_Board_Template_Loader::get_template_part( 'single-job_listing/detail' ); ?>
 
 			<?php do_action( 'wp_job_board_before_job_content', $post->ID ); ?>
-			
-			<!-- job description -->
+
+			<!-- job descriptionnn -->
 			<div class="job-detail-description">
 				<h3 class="title-detail-job"><?php esc_html_e('Job Description', 'careerup'); ?></h3>
 				<div class="inner">
@@ -36,7 +36,7 @@ global $post;
 
 			<?php do_action( 'wp_job_board_after_job_content', $post->ID ); ?>
 		</div>
-		
+
 		<?php if ( is_active_sidebar( 'job-single-sidebar-v2' ) ): ?>
 			<div class="col-md-4 col-xs-12">
 		   		<?php dynamic_sidebar( 'job-single-sidebar-v2' ); ?>
@@ -46,7 +46,7 @@ global $post;
 		<div class="hidden-lg hidden-md col-xs-12">
 			<?php echo WP_Job_Board_Template_Loader::get_template_part( 'single-job_listing/releated' ); ?>
 		</div>
-	   	
+
 	</div>
 
 </article><!-- #post-## -->
