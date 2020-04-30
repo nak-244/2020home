46008e77260687cb2771f7d3027461e94572cf07
nak-244 <t09024656860@gmail.com>
2020-04-30 13:58:49 +0900

    Update reply-section.php

--- wp-private-message/templates/reply-section.php
+++ wp-private-message/templates/reply-section.php
@@ -35,7 +35,9 @@ if ( $selected_post ) {
           <div class="message-title"><?php echo esc_html($selected_post->post_title); ?></div>
         </div>
     </div>
-    <a href="javascript:void(0);" class="delete-message-btn pull-right" data-id="<?php echo esc_attr($selected_post->ID); ?>" data-nonce="<?php echo esc_attr(wp_create_nonce( 'wp-private-message-delete-message-nonce' )); ?>"><i class="flaticon-dustbin"></i>あああ<?php esc_html_e('Delete Conversation', 'wp-private-message'); ?></a>
+		<!-- ああ -->
+    <a href="javascript:void(0);" class="delete-message-btn pull-right" data-id="<?php echo esc_attr($selected_post->ID); ?>" data-nonce="<?php echo esc_attr(wp_create_nonce( 'wp-private-message-delete-message-nonce' )); ?>"><i class="flaticon-dustbin"></i><?php esc_html_e('Delete Conversation', 'wp-private-message'); ?></a>
+
   </div>
   <div class="content-box-white">
     <div class="list-replies-inner">
