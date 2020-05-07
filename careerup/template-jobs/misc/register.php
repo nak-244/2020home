346008f4697f7ac6cfa50590f86cc1d5e1656d13
nak-244 <t09024656860@gmail.com>
2020-05-07 14:28:59 +0900

    Update register.php

--- careerup/template-jobs/misc/register.php
+++ careerup/template-jobs/misc/register.php
@@ -21,7 +21,7 @@ wp_enqueue_style('select2');
 						<li class="active"><input id="cadidate" type="radio" name="role" value="wp_job_board_candidate" checked="checked">
 							<label for="cadidate">求職者登録フォーム</label>
 						</li>
-						<!-- <li><input type="radio" id="employer" name="role" value="wp_job_board_employer"><label for="employer"><?php esc_html_e('Employer', 'careerup'); ?></label></li> -->
+						<li><input type="radio" id="employer" name="role" value="wp_job_board_employer"><label for="employer"><?php esc_html_e('Employer', 'careerup'); ?></label></li>
 					</ul>
 				</div>
 				<div class="form-group">
