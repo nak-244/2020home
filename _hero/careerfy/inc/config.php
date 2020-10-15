fa972f9606ae216476847eb374b6fc1e4318081b
nak-244 <t09024656860@gmail.com>
2020-10-15 13:56:03 +0900

    up

---
+++ _hero/careerfy/inc/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Careerfy Theme Config.
+ *
+ * @package Careerfy
+ */
+define("CAREERFY_VERSION", "3.3.0");
+define("WP_JOBSEARCH_VERSION", "1.4.5");
+
+function careerfy_framework_options() {
+    global $careerfy_framework_options;
+    return $careerfy_framework_options;
+}
