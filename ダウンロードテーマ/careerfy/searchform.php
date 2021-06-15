9717508cb5e4a479b69e9d15f846577d81128117
nak-244 <t09024656860@gmail.com>
2021-06-15 09:19:39 +0900

    del

--- ダウンロードテーマ/careerfy/searchform.php
+++
@@ -1,6 +0,0 @@
-<form method="get" action="<?php echo esc_url(home_url('/')); ?>">
-    <ul>
-        <li><input type="text" name="s" placeholder="<?php esc_html_e('Keywords...', 'careerfy'); ?>"></li>
-        <li><input type="submit" value="<?php esc_html_e('Search', 'careerfy'); ?>"></li>
-    </ul>
-</form>
